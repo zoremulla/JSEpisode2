fb560c3bcda6d54a784fdec6187eb311f813aaf6
Octowl <alsaff1987@gmail.com>
2018-03-15 12:07:45 +0300

    Starter file

--- pairs.js
+++ pairs.js
@@ -1,5 +1,23 @@
 function pairs(names) {
-  return names;
+
+  // Your code goes here
+  
 }
 
 export default pairs;
+
+/**********************************************
+* We've including this handy method for you.
+* It will remove a random element from an array
+* and return it to you.
+*
+* Example:
+*
+* let array = [1, 2, 3, 4];
+* let random = array.getRandom();  // randomly returns something from the array. e.g. 3
+* console.log(random); // 3 the random element
+* console.log(array);  // [1, 2, 4] missing the random element
+************************************************/
+Array.prototype.getRandom = function () {
+  return this.splice(Math.floor(Math.random()*this.length), 1)[0];
+}
