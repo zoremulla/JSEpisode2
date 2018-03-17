6b4ee424769a6aa01f7cd2bdc9a6c9c6cf476f4d
Octowl <alsaff1987@gmail.com>
2018-03-17 20:59:23 +0300

    Add description of pairs()

--- pairs.js
+++ pairs.js
@@ -1,22 +1,41 @@
+/**************************************************************
+* pairs(names):
+*
+* - It accepts an array of names:
+*       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish'])
+*
+* - It returns a randomized pairing of names:
+*       [['Mishmish', 'Asis'], ['Fawas', 'Hamsa']]
+*
+* - It can handle an odd number of names:
+*       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein])
+*       returns [['Mishmish', 'Fawas'], ['Asis', 'Hussein'], ['Hamsa']]
+*
+* - It returns an empty array if it gets passed an empty array:
+*       pairs([]) returns []
+*
+* - It returns an empty array if it gets passed nothing:
+*       pairs() returns []
+****************************************************************/
 function pairs(names) {
-
   // Your code goes here
-  
 }
 
 export default pairs;
 
 /**********************************************
+* READ ME!!!!
+*
 * We've including this handy method for you.
 * It will remove a random element from an array
 * and return it to you.
 *
-* Example:
+* Example Usage:
 *
-* let array = [1, 2, 3, 4];
-* let random = array.getRandom();  // randomly returns something from the array. e.g. 3
-* console.log(random); // 3 the random element
-* console.log(array);  // [1, 2, 4] missing the random element
+* let numbers = [1, 2, 3, 4];
+* let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
+* console.log(random); // 3 (the random element)
+* console.log(numbers);  // [1, 2, 4] (missing the random element)
 ************************************************/
 Array.prototype.getRandom = function () {
   return this.splice(Math.floor(Math.random()*this.length), 1)[0];
