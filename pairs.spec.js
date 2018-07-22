4757d848ba352dd16d71f7a1ba11be6332973f3a
Fawaz <q8fawazo@hotmail.com>
2018-07-22 06:02:30 -0700

    Reformating test file

--- pairs.spec.js
+++ pairs.spec.js
@@ -1,49 +1,54 @@
 /**************************
-*
-* THIS IS A TESTING FILE
-*
-* DO NOT MODIFY THIS FILE
-*
-***************************/
+ *
+ * THIS IS A TESTING FILE
+ *
+ * DO NOT MODIFY THIS FILE
+ *
+ ***************************/
 
-import pairs from './pairs';
+import pairs from "./pairs";
 
-test('should return an empty array when passed nothing', () => {
-  const result = pairs()
-	expect(result).toEqual([]);
-});
+describe("pairs()", () => {
+  test("returns an empty array when passed nothing", () => {
+    const result = pairs();
+    expect(result).toEqual([]);
+  });
 
-test('should return an empty array when passed an empty array', () => {
-  const result = pairs([])
-	expect(result).toEqual([]);
-});
+  test("returns an empty array when passed an empty array", () => {
+    const result = pairs([]);
+    expect(result).toEqual([]);
+  });
 
-test('should return an array with a single pair of items when passed an array with just two items', () => {
-  const names = ['asis', 'hamsa'];
-  const result = pairs(names);
-	expect(result.length).toBe(1);
-	expect(result[0].length).toBe(2);
-	expect(result[0].includes('asis')).toBe(true);
-	expect(result[0].includes('hamsa')).toBe(true);
-});
+  test("returns an array with a single pair of items when passed an array with just two items", () => {
+    const names = ["asis", "hamsa"];
+    const result = pairs(names);
+    expect(result.length).toBe(1);
+    expect(result[0].length).toBe(2);
+    expect(result[0].includes("asis")).toBe(true);
+    expect(result[0].includes("hamsa")).toBe(true);
+  });
 
-test('should return an array of pairs when passed multiple names', () => {
-  const names = ['asis', 'hamsa', 'fawas', 'mishmish'];
-  const result = pairs(names);
-  expect(result.length).toBe(2);
-  expect(result.every(pair => pair.length == 2)).toBe(true);
-});
+  test("returns an array of pairs when passed multiple names", () => {
+    const names = ["asis", "hamsa", "fawas", "mishmish"];
+    const result = pairs(names);
+    expect(result.length).toBe(2);
+    expect(result.every(pair => pair.length == 2)).toBe(true);
+  });
 
-test('pairs should be randomized', () => {
-  let names = ['asis', 'hamsa', 'fawas', 'mishmish'];
-  let pairsets = [...Array(10)].map(() => pairs(names.slice(0)));
-  expect(pairsets.every(pairset =>
-    JSON.stringify(pairset) === JSON.stringify(pairsets[0]))).toBe(false);
-});
+  test("is randomized", () => {
+    let names = ["asis", "hamsa", "fawas", "mishmish"];
+    let pairsets = [...Array(10)].map(() => pairs(names.slice(0)));
+    expect(
+      pairsets.every(
+        pairset => JSON.stringify(pairset) === JSON.stringify(pairsets[0])
+      )
+    ).toBe(false);
+  });
 
-test('should be able to handle an odd number of names, the last array should contain only one name', () => {
-  let names = ['asis', 'hamsa', 'fawas', 'mishmish', 'hussein'];
-  let result = pairs(names)
-  let last = result.pop()
-  expect(last.length).toBe(1);
+  test("can handle an odd number of names, the last array contains only one name", () => {
+    let names = ["asis", "hamsa", "fawas", "mishmish", "hussein"];
+    let result = pairs(names);
+    let last = result.pop();
+    expect(last.length).toBe(1);
+  });
 });
